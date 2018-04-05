3f9268b8257c7548ece796b3c8662798ed456f24
slowjazz <kenkraken@gmail.com>
2018-04-04 22:55:53 -0500

    Updated model file to take string in console input

--- model.py
+++ model.py
@@ -1,11 +1,18 @@
-i = input() # expecting single string of news article text
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 
-tfidf_vectorizer = pickle.load("tfidf_vectorizer.pckl")
+i = [input()] # expecting single string of news article text
+print(i)
+# from sklearn.linear_model import PassiveAggressiveClassifier
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
+f1 = open("tfidf_vectorizer.pckl","rb")
+tfidf_vectorizer = pickle.load(f1)
 v = tfidf_vectorizer.transform(i) #vectorized string
-model = pickle.load("PAC_model.pckl")
+print(v)
+
+f2 = open("PAC_model.pckl","rb")
+model = pickle.load(f2)
 pred = model.predict(v)
 print(pred)
+f1.close()
+f2.close()
