6ec5d088d5b37dbd2a73b64153631cbddf6510e4
slowjazz <kenkraken@gmail.com>
2018-04-12 18:16:55 -0500

    Cleaned up model.py

--- model.py
+++ model.py
@@ -1,15 +1,12 @@
 import pickle
 
 i = [input()] # expecting single string of news article text
-print(i)
 # from sklearn.linear_model import PassiveAggressiveClassifier
 # from sklearn.feature_extraction.text import TfidfVectorizer
 
 f1 = open("tfidf_vectorizer.pckl","rb")
 tfidf_vectorizer = pickle.load(f1)
 v = tfidf_vectorizer.transform(i) #vectorized string
-print(v)
-
 f2 = open("PAC_model.pckl","rb")
 model = pickle.load(f2)
 pred = model.predict(v)
